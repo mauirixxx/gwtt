3df1dd0c52b5d3ba71fcde9998b49168a4969e8a
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-18 00:13:39 -1000

    reverting all changes
    i think i need to test for 0 results via mysqli_num_rows. now to figure out how.

--- gw-pull.php
+++ gw-pull.php
@@ -10,16 +10,12 @@ if (!$result = $con->query($sql)){
 	die ('There was an error running the query [' . $con->error . ']');
 }
 while ($row = $result->fetch_array()){
-	if (empty($row['charname'])){
-		echo 'No data available for that toon';
+	echo 'On ' . $row['historydate'] . ', "' . $row['charname'] . '" got ' . $row['goldrec'] . 'GP and ';
+	if (is_null($row['material'])) {
+		echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
 	} else {
-		echo 'On ' . $row['historydate'] . ', "' . $row['charname'] . '" got ' . $row['goldrec'] . 'GP and ';
-		if (is_null($row['material'])) {
-			echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
-		} else {
-			echo 'a ' . $row['material'] . '!<BR />';
-		} //add another else statement to cover RUNES here
-	}
+		echo 'a ' . $row['material'] . '!<BR />';
+	} //add another else statement to cover RUNES here
 }
 ?>
 
