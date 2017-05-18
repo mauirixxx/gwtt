613b2e77b70b2ce62e5f9ef0e20911dc190f7efd
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-18 00:34:09 -1000

    dealing with runes in the mix

--- gw-pull.php
+++ gw-pull.php
@@ -12,11 +12,14 @@ if (!$result = $con->query($sql)){
 if (mysqli_num_rows($result) > 0) {
 	while ($row = $result->fetch_array()){
 		echo 'On ' . $row['historydate'] . ', "' . $row['charname'] . '" got ' . $row['goldrec'] . 'GP and ';
+		if ($row['itemtype'] == "Rune") {
+			echo 'a ' . $row['itemtype'] . ' of ' . $row['runetype'] . '!<BR />';
+		} else {
 		if (is_null($row['material'])) {
 			echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
 		} else {
 			echo 'a ' . $row['material'] . '!<BR />';
-		} //add another else statement to cover RUNES here
+		} //add another else statement to cover RUNES here. or up there.
 	}
 } else {
 	echo 'no data to display for that character';
