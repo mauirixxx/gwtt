f0382b9206b14e5e306f95494740399d38369000
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-17 23:26:02 -1000

    need to make a form for actual submission

---
+++ gw-toon.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'gw-connect.php';
+$con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
+if ($con->connect_errno > 0){
+	die ('Unable to connect to database [' . $db->connect_errno . ']');
+}
+$sql = "SELECT charname FROM `playername` WHERE `userid` = '1' ORDER BY `charname` ASC"; //need to make userid a variable
+if (!$result = $con->query($sql)){
+	die ('There was an error running the query [' . $con->error . ']');
+}
+while ($row = $result->fetch_array()){
+	echo 'Please select a character to continue' . $row['charname'];
+}
+?>
