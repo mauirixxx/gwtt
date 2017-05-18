4f953bb0f8a6d6be805a08020f729fa363d46a2c
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-17 23:52:05 -1000

    if the toonhas no data, let someone know

--- gw-pull.php
+++ gw-pull.php
@@ -10,12 +10,16 @@ if (!$result = $con->query($sql)){
 	die ('There was an error running the query [' . $con->error . ']');
 }
 while ($row = $result->fetch_array()){
-	echo 'On ' . $row['historydate'] . ', "' . $row['charname'] . '" got ' . $row['goldrec'] . 'GP and ';
-	if (is_null($row['material'])) {
-		echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
+	if ($row['charname'] == ''){
+		echo 'No data available for $cnameid';
 	} else {
-		echo 'a ' . $row['material'] . '!<BR />';
-	} //add another else statement to cover RUNES here
+		echo 'On ' . $row['historydate'] . ', "' . $row['charname'] . '" got ' . $row['goldrec'] . 'GP and ';
+		if (is_null($row['material'])) {
+			echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
+		} else {
+			echo 'a ' . $row['material'] . '!<BR />';
+		} //add another else statement to cover RUNES here
+	}
 }
 ?>
 
