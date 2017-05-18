e5520d88a8a60a34940ffd021c4805ea165853ce
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-18 00:35:30 -1000

    forgot a bracket I think

--- gw-pull.php
+++ gw-pull.php
@@ -15,10 +15,11 @@ if (mysqli_num_rows($result) > 0) {
 		if ($row['itemtype'] == "Rune") {
 			echo 'a ' . $row['itemtype'] . ' of ' . $row['runetype'] . '!<BR />';
 		} else {
-		if (is_null($row['material'])) {
-			echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
-		} else {
-			echo 'a ' . $row['material'] . '!<BR />';
+			if (is_null($row['material'])) {
+				echo 'a ' . $row['itemrarity'] . ' req' . $row['itemreq'] . ' ' . $row['itemattribute'] . ' ' . $row['itemtype'] . ' named something stupid <BR />';
+			} else {
+				echo 'a ' . $row['material'] . '!<BR />';
+			}
 		} //add another else statement to cover RUNES here. or up there.
 	}
 } else {
