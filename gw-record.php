a34c19a999b947a64d7cdabf1836082c512cce15
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-18 14:27:14 -1000

    html hell

--- gw-record.php
+++ gw-record.php
@@ -15,6 +15,7 @@ while ($row = $result->fetch_array()){
 	$weapattr = $row['weaponattribute'];
 	echo '<OPTION VALUE="' . $attrid . '">' . $weapattr . '</OPTION>';
 }
+echo '</SELECT> ';
 $sqlweaprare = "SELECT * FROM `listrarity` ORDER BY `rareid` ASC";
 if (!$result = $con->query($sqlweaprare)){
 	die ('There was an error running the query [' . $con->error . ']');
@@ -25,4 +26,5 @@ while ($row = $result->fetch_array()){
 	$rarity = $row['rarity'];
 	echo '<OPTION VALUE="' . $rareid . '">' . $rarity . '</OPTION>';
 }
+echo '</SELECT> ';
 ?>
