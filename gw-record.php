558420b519fadfb4e011c9543b732467bce37bac
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-18 16:47:29 -1000

    dunno what

--- gw-record.php
+++ gw-record.php
@@ -2,6 +2,8 @@
 <?php
 include_once 'gw-connect.php';
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
+#$toonid = mysqli_real_escape_string($con, $_POST['playerid']); //enable this after character selection is working
+$toonid = 'Chrissi Chan';
 if ($con->connect_errno > 0){
 	die ('Unable to connect to database [' . $db->connect_errno . ']');
 }
@@ -16,6 +18,18 @@ while ($row = $result->fetch_array()){
 	echo '<OPTION VALUE="' . $attrid . '">' . $weapattr . '</OPTION>';
 }
 echo '</SELECT>';
+$sqlweaprare = "SELECT * FROM `listrarity` ORDER BY `rareid` ASC";
+if (!$result = $con->query($sqlweaprare)){
+	die ('There was an error running the query [' . $con->error . ']');
+}
+echo '<SELECT NAME="rare">';
+while ($row = $result->fetch_array()){
+	$rareid = $row['rareid'];
+	$rarity = $row['rarity'];
+	echo '<OPTION VALUE="' . $rareid . '">' . $rarity . '</OPTION>';
+}
+echo '</SELECT><BR />';
+
 $sqlweaprare = "SELECT * FROM `listrarity` ORDER BY `rareid` ASC";
 if (!$result = $con->query($sqlweaprare)){
 	die ('There was an error running the query [' . $con->error . ']');
