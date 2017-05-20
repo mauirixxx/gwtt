2328dec7bfeb1ae51855d900d9e209522d67939d
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-20 13:06:53 -1000

    cut & paste fail, added comments

--- gw-record.php
+++ gw-record.php
@@ -7,11 +7,9 @@ $toonid = 'Chrissi Chan';
 if ($con->connect_errno > 0){
 	die ('Unable to connect to database [' . $db->connect_errno . ']');
 }
-$sqlweapattr = "SELECT * FROM `listattribute` ORDER BY `weapattrid` ASC";
-if (!$result = $con->query($sqlweapattr)){
-	die ('There was an error running the query [' . $con->error . ']');
-}
-echo ' At <insert location SQL code here> a/an ';
+
+echo 'At |insert location SQL code here| a/an ';
+//code for weapon attribute requirment
 $sqlweapreq = "SELECT * FROM `listreq` ORDER BY `req` ASC";
 if (!$result = $con->query($sqlweapreq)){
 	die ('There was an error running the query [' . $con->error . ']');
@@ -22,6 +20,8 @@ while ($row = $result->fetch_array()){
 	echo '<OPTION VALUE="' . $reqid . '">' . $reqid . '</OPTION>';
 }
 echo '</SELECT>';
+
+//code for white blue purple etc
 $sqlweaprare = "SELECT * FROM `listrarity` ORDER BY `rareid` ASC";
 if (!$result = $con->query($sqlweaprare)){
 	die ('There was an error running the query [' . $con->error . ']');
@@ -33,6 +33,12 @@ while ($row = $result->fetch_array()){
 	echo '<OPTION VALUE="' . $rareid . '">' . $rarity . '</OPTION>';
 }
 echo '</SELECT>';
+
+//code for what attribute the weapon is (command, axe mastery, energy storage, etc
+$sqlweapattr = "SELECT * FROM `listattribute` ORDER BY `weapattrid` ASC";
+if (!$result = $con->query($sqlweapattr)){
+	die ('There was an error running the query [' . $con->error . ']');
+}
 echo '<SELECT NAME="attribute">';
 while ($row = $result->fetch_array()){
 	$attrid = $row['weapattrid'];
