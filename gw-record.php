c0771ebf733e2ff52b6d513efd95e21fd6b9700f
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-21 21:37:30 -1000

    dug deeper into input type number html5 madness

--- gw-record.php
+++ gw-record.php
@@ -72,7 +72,7 @@ if ($whatdropped == "1"){
 		echo '<OPTION VALUE="' . $typeid . '">' . $weapon . '</OPTION>';
 	}
 	echo '</SELECT>';
-	echo ' and <INPUT TYPE="NUMBER" NAME="droppedgold" SIZE="4" MAXLENGTH="4"> gold pieces.';
+	echo ' and <INPUT TYPE="NUMBER" NAME="droppedgold" SIZE="4" MIN="1" MAX="9999"> gold pieces.';
 	echo '<INPUT TYPE="HIDDEN" NAME="droptype" VALUE="1"><INPUT TYPE="HIDDEN" NAME="location" VALUE="' . $locid .'">';
 	echo ' <BR /><CENTER><INPUT TYPE="SUBMIT" VALUE="Click me!"></FORM></CENTER><BR />';
 } else if ($whatdropped == "2"){
