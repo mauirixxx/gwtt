2c4219107a3b371301a7efbc23f8a2ad3326c92e
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-21 23:26:06 -1000

    found the missing closure

--- gw-insert.php
+++ gw-insert.php
@@ -9,11 +9,11 @@ $toonid = mysqli_real_escape_string($con, $_POST['chartoon']; //this is the id o
 if ($droptype == 1){
 	$rarity = mysqli_real_escape_string($con, $_POST['rare']);
 	$req = mysqli_real_escape_string($con, $_POST['requirement']);
-	$attrib = mysqli_real_escape_string($con, $_POST['attribute'];
+	$attrib = mysqli_real_escape_string($con, $_POST['attribute']);
 	$weap = mysqli_real_escape_string($con, $_POST['weapon']);
-	//$itname = mysqli_real_escape_string($con, $_POST['itemname']);
-	//echo 'SQL code to run: "INSERT INTO `history` (historydate, charnameid, locationid, goldrec, itemreq, itemtype, itemattribute, itemrarity, itemname) VALUES	(\'$variable-date-of-treasure\', ' . $toonid . ', ' . $locid . ', ' . $gold . ', ' . $req . ', ' . $weap . ', '$variable-attribute-of-weapon', '$variable-rarity-of-weapon', '$variable-name-of-weapon');"';
-	echo 'Well something broke somewhere!<BR />';
+	$itname = mysqli_real_escape_string($con, $_POST['itemname']);
+	echo 'SQL code to run: "INSERT INTO `history` (historydate, charnameid, locationid, goldrec, itemreq, itemtype, itemattribute, itemrarity, itemname) VALUES	(\'$variable-date-of-treasure\', ' . $toonid . ', ' . $locid . ', ' . $gold . ', ' . $req . ', ' . $weap . ', '$variable-attribute-of-weapon', '$variable-rarity-of-weapon', '$variable-name-of-weapon');"';
+	echo '<BR />Well something broke somewhere!<BR />';
 } else if ($droptype == 2){
 	echo 'drop was a rare material!<BR />';
 } else if ($droptype == 3){
