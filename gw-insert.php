04f4e6cf69735f481cdecb2fab34068ccd9fead9
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-21 20:54:51 -1000

    gold be dropping!
    using an html 5 specific input option of "number" only, in theory limited to 4 digits.
    
    here we go!

--- gw-insert.php
+++ gw-insert.php
@@ -2,9 +2,12 @@
 include_once 'gw-connect.php';
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
 //post data section
+# just testing max gold value - should be 9999
+$gold = $_POST['droppedgold'];
 $droptype = mysqli_real_escape_string($con, $_POST['droptype']); //this dictates if the drop was a weapon/rune/material
 $locid = mysqli_real_escape_string($con, $_POST['location']); //this is `treasurelocation`.`treasureid` in the database
 echo 'The droptype variable is set to ' . $droptype . '<BR />';
 echo 'The locid variable is set to ' . $locid . '<BR />';
-echo 'Return to <A HREF="gw-record.php">data recording</A> page';
+echo 'The amount of gold dropped was ' . $gold . '<BR />';
+echo '<BR />Return to <A HREF="gw-record.php">data recording</A> page';
 ?>
