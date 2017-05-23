d9e981ec2fd2c092ec6a72a2a0c739aad8b0636c
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-23 12:23:41 -1000

    prepping toon selection

--- gw-toon.php
+++ gw-toon.php
@@ -1,6 +1,8 @@
 <?php
 include_once 'gw-connect.php';
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
+$userid = 1; //need to actually pull this info from cookie/session (preferable)
+$whattoon = mysqli_real_escape_string($con, $_POST['cname']);
 if ($con->connect_errno > 0){
 	die ('Unable to connect to database [' . $db->connect_errno . ']');
 }
@@ -8,12 +10,17 @@ $sql = "SELECT playerid, charname FROM `playername` WHERE `userid` = '1' ORDER B
 if (!$result = $con->query($sql)){
 	die ('There was an error running the query [' . $con->error . ']');
 }
-echo '<FORM METHOD="POST" NAME="cselect" ACTION="gw-pull.php">';
-echo '<SELECT NAME="cname">';
-while ($row = $result->fetch_array()){
-	$charid = $row['playerid'];
-	$charname = $row['charname'];
-	echo '<OPTION VALUE="' . $charid . '">' . $charname . '</OPTION>';
+# experimental stuff
+if (isset($whattoon)){
+	echo 'Options for what to do after character selection goes here <BR />';
+} else {
+	echo '<FORM METHOD="POST" NAME="cselect" ACTION="gw-toon.php">';
+	echo '<SELECT NAME="cname">';
+	while ($row = $result->fetch_array()){
+		$charid = $row['playerid'];
+		$charname = $row['charname'];
+		echo '<OPTION VALUE="' . $charid . '">' . $charname . '</OPTION>';
+	}
+	echo '</SELECT><INPUT TYPE="SUBMIT" VALUE="Choose Toon"></FORM>';
 }
-echo '</SELECT><INPUT TYPE="SUBMIT" VALUE="Choose Toon"></FORM>';
 ?>
