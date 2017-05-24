a0a227e2946cf483823e7a07f2fc943c29f496ca
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-23 19:47:22 -1000

    added notes so I don't get confused

--- gw-action.php
+++ gw-action.php
@@ -2,10 +2,10 @@
 $toonid = $_POST['playerid'];
 $action = $_POST['gwaction'];
 echo'<TITLE>WHY ARE YOU HERE?</TITLE>';
-if ($action == 1){
+if ($action == 1){ //insert dropped items data
 	echo '<BODY onload="document.record.submit()">';
 	echo '<FORM METHOD="POST" ACTION="gw-record.php" NAME="record"><INPUT TYPE="HIDDEN" NAME="playerid" VALUE="'. $toonid . '"><INPUT TYPE="SUBMIT" ID="clkRecord"></FORM></BODY>';
-} else if ($action == 2){
+} else if ($action == 2){ //view history of dropped items
 	echo '<BODY onload="document.insert.submit()">';
 	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $toonid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
 } else {
