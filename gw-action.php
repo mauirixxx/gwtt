d29d7735ea3a0dc97a73c8042f9e365eb96ac459
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-23 15:29:07 -1000

    forgot to rename a "name"

--- gw-action.php
+++ gw-action.php
@@ -6,7 +6,7 @@ if ($action == 1){
 	echo '<FORM METHOD="POST" ACTION="gw-record.php" NAME="record"><INPUT TYPE="HIDDEN" NAME="playerid" VALUE="'. $toonid . '"><INPUT TYPE="SUBMIT" ID="clkRecord"></FORM></BODY>';
 } else if ($action == 2){
 	echo '<body onload="javascript:submitForm()">';
-	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cname" VALUE="' . $toonid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
+	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $toonid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
 } else {
 	echo 'You shouldn\'t be seeing this, something went horribly horribly wrong!';
 }
