b7601bdc61c63ab4b0fcc0f5d791d7bb7f3fbf6f
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-23 23:39:34 -1000

    moved submit action from gw-record to gw-location
    so I can actually choose a location in which to record data from

--- gw-action.php
+++ gw-action.php
@@ -4,7 +4,7 @@ $action = $_POST['gwaction'];
 echo'<TITLE>Redirecting ...</TITLE>';
 if ($action == 1){ //insert dropped items data
 	echo '<BODY onload="document.record.submit()">';
-	echo '<FORM METHOD="POST" ACTION="gw-record.php" NAME="record"><INPUT TYPE="HIDDEN" NAME="playerid" VALUE="'. $toonid . '"><INPUT TYPE="SUBMIT" ID="clkRecord"></FORM></BODY>';
+	echo '<FORM METHOD="POST" ACTION="gw-location.php" NAME="record"><INPUT TYPE="HIDDEN" NAME="playerid" VALUE="'. $toonid . '"><INPUT TYPE="SUBMIT" ID="clkRecord"></FORM></BODY>';
 } else if ($action == 2){ //view history of dropped items
 	echo '<BODY onload="document.insert.submit()">';
 	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $toonid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
