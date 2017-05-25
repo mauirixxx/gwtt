7f6b6088bcef23d03b89c39d9103f47d561c4337
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 22:36:20 -1000

    starting to move data to session data

--- gw-action.php
+++ gw-action.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$playerid = $_SESSION['playerid'];
 $toonid = $_POST['playerid'];
 $action = $_POST['gwaction'];
 echo'<TITLE>Redirecting ...</TITLE>';
@@ -7,7 +9,7 @@ if ($action == 1){ //insert dropped items data
 	echo '<FORM METHOD="POST" ACTION="gw-location.php" NAME="record"><INPUT TYPE="HIDDEN" NAME="playerid" VALUE="'. $toonid . '"><INPUT TYPE="SUBMIT" ID="clkRecord"></FORM></BODY>';
 } else if ($action == 2){ //view history of dropped items
 	echo '<BODY onload="document.insert.submit()">';
-	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $toonid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
+	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $playerid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
 } else {
 	echo 'You shouldn\'t be seeing this, something went horribly horribly wrong!';
 }
