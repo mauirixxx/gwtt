767b29a0b38e998a53e784f30f8f6790d9e4ff54
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 17:01:59 -1000

    attempting session data usage

--- gw-toon.php
+++ gw-toon.php
@@ -1,12 +1,12 @@
 <?php
 include_once 'gw-connect.php';
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
-$userid = 1; //need to actually pull this info from cookie/session (preferable)
+$userid = $_SESSION['userid']; //need to actually pull this info from cookie/session (preferable)
 $whattoon = mysqli_real_escape_string($con, $_POST['cnameid']);
 if ($con->connect_errno > 0){
 	die ('Unable to connect to database [' . $db->connect_errno . ']');
 }
-if ($whattoon == "0"){
+if ($whattoon == "0" or $whattoon == ""){
 	$sql = "SELECT playerid, charname FROM `playername` WHERE `userid` = '1' ORDER BY `charname` ASC"; //need to make userid a variable
 	if (!$result = $con->query($sql)){
 		die ('There was an error running the query [' . $con->error . ']');
