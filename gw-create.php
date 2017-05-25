8a514ed667b1556accb5c35e64a0b2396f845594
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-25 12:16:32 -1000

    initial file creation
    lot's of placeholder stuff

---
+++ gw-create.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<TITLE>Character Creation</TITLE>
+</HEAD>
+<BODY>
+<?php
+echo '<CENTER>Character creation isn\'t enabled yet!<BR />';
+echo 'Return to <A HREF="gw-index.php">home</A>.</CENTER>';
+?>
+</BODY>
+</HTML>
