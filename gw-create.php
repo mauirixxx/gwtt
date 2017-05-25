e5afb412bd401256975d3cb7fdbb65702d3e80cf
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-25 12:23:38 -1000

    remembered to add session_start this time!

--- gw-create.php
+++ gw-create.php
@@ -5,6 +5,8 @@
 </HEAD>
 <BODY>
 <?php
+session_start();
+$userid = $_SESSION['userid'];
 echo '<CENTER>Character creation isn\'t enabled yet!<BR />';
 echo 'Return to <A HREF="gw-index.php">home</A>.</CENTER>';
 ?>
