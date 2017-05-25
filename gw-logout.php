dc22df5a4e6fc4d47c4017081d480eec2bbbbe5b
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 20:43:00 -1000

    missed a link

--- gw-logout.php
+++ gw-logout.php
@@ -2,7 +2,7 @@
 if (isset($_POST['logout'])){
 	session_unset();
 	session_destroy();
-	header("refresh:5;url=login.php");
+	header("refresh:5;url=gw-login.php");
 	echo '<CENTER>You have been logged out ...<BR />Returning to login screen in a few seconds</CENTER>';
 } else {
 	echo 'Something went wrong, you haven\'t been logged out!';
