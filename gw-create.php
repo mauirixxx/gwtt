a048187b9805f0c1c4dc522c30428fab50375b2c
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-25 13:34:33 -1000

    hopefully i didn't forget anything
    who am i kidding I always forget stuff

--- gw-create.php
+++ gw-create.php
@@ -6,9 +6,40 @@
 <BODY>
 <?php
 session_start();
+include_once 'gw-connect.php';
+$con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
+$createnew = mysqli_real_escape_string($con, $_POST['docreate']);
 $userid = $_SESSION['userid'];
-echo '<CENTER>Character creation isn\'t enabled yet!<BR />';
-echo 'Return to <A HREF="gw-index.php">home</A>.</CENTER>';
+echo '<CENTER>Character creation isn\'t enabled yet!<BR />Your userid is ' . $userid . '';
+if ($createnew === "1"){
+	$cname = mysqli_real_escape_string($con, $_POST['cname']);
+	$bdate = mysqli_real_escape_string($con, $_POST['bdate']);
+	$profid = mysqli_real_escape_string($con, $_POST['professionid']);
+	$sqlcreate = "INSERT INTO `playername` (charname, birthdate, userid, professionid) VALUES ('$cname', '$bdate', $userid, $profid)";
+	echo 'Character creation database insertion code here';
+} else {
+	echo 'Form creation code goes here';
+	echo '<CENTER><FORM METHOD="POST" ACTION="gw-create.php"><INPUT TYPE="HIDDEN" NAME="docreate" VALUE="1">';
+	echo 'Character name: <INPUT TYPE="TEXT" NAME="cname" MAXLENGTH="19" SIZE="20"><BR />';
+	echo 'Birthdate: <INPUT NAME="bdate" TYPE="DATE" PLACEHOLDER="2005-04-28"><BR />';
+	echo '';
+	$sqlprofession = "SELECT * FROM (SELECT * FROM listruneprofessions ORDER BY runeprofid DESC LIMIT 10) sub ORDER BY runeprofid ASC";
+	if (!$result = $con->query($sqlprofession)){
+		die ('There was an error running the query [' . $con->error . ']');
+	}
+	echo 'Profession: <SELECT NAME="profid">';
+	echo '<OPTION SELECTED DISABLED>Choose Profession</OPTION>';
+	while ($row = $result->fetch_array()){
+		$professionid = $row['runeprofid'];
+		$profession = $row['runeprofession'];
+		echo '<OPTION VALUE="' . $professionid . '">' . $profession . '</OPTION>';
+	}
+	echo '</SELECT><BR /><BR /> ';
+	echo '<INPUT TYPE="SUBMIT" VALUE="Create character ..."></FORM>';
+}
 ?>
+<BR /><BR />
+Return to <A HREF="gw-index.php">home</A>.
+</CENTER>
 </BODY>
 </HTML>
