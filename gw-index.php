460efcdfaf29a07310bd1f6d74e0d212fc25935d
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 20:20:34 -1000

    attempting a login form
    first crack

--- gw-index.php
+++ gw-index.php
@@ -1,8 +1,11 @@
 <?php
 // tie everything together from here hopefully, might not even need the connection info?
-include_once 'gw-connect.php';
-$con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
-if ($con->connect_errno > 0){
-	die ('Unable to connect to database [' . $db->connect_errno . ']');
-}
+session_start();
+if (isset($_SESSION['userid']) && ($_SESSION['access'])){
+	echo 'Proceed to character selection <A HREF="gw-toon.php">here</A><BR>'; //really should automate this
+} else {
+	echo '<TITLE>Login Required</TITLE><BODY>';
+	echo '<CENTER><FORM ACTION="gw-login.php" METHOD="POST">Username:<INPUT TYPE="TEXT" NAME="username" SIZE="20"><BR />';
+	echo 'Password:<INPUT TYPE="PASSWORD" NAME="password" SIZE="20"><BR />';
+	echo '<INPUT TYPE="SUBMIT" VALUE="Login ..."></FORM></CENTER>';
 ?>
