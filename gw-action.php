c08f3bbebe5e198f704fc510ed9aaddb6b6f7467
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 23:53:35 -1000

    beginnings of toon creation

--- gw-action.php
+++ gw-action.php
@@ -9,7 +9,11 @@ if ($action == 1){ //insert dropped items data
 } else if ($action == 2){ //view history of dropped items
 	echo '<BODY onload="document.insert.submit()">';
 	echo '<FORM METHOD="POST" ACTION="gw-pull.php" NAME="insert"><INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="' . $playerid . '"><INPUT TYPE="SUBMIT" ID="clkInsert"></FORM></BODY>';
-} else {
+} else if ($action == 3){ //toon creation page here
+	echo '<BODY onload="document.createtoon.submit()">';
+	echo 'Character creation is currently disabled!<BR><BR>Click <A HREF="gw-index.php">here</A> to go to the main page';
+	//echo '<FORM METHOD="POST" ACTION="gw-create.php" NAME="createtoon"><INPUT TYPE="SUBMIT"></FORM></BODY>'; //enable this and modify it when gw-create.php is made
+{
 	echo 'You shouldn\'t be seeing this, something went horribly horribly wrong!';
 }
 ?>
