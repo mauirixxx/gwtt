f16f9d378c29f77947cb21a19903318dfb6189ae
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 20:31:31 -1000

    forgot to close something off. again.

--- gw-index.php
+++ gw-index.php
@@ -8,4 +8,5 @@ if (isset($_SESSION['userid']) && ($_SESSION['access'])){
 	echo '<CENTER><FORM ACTION="gw-login.php" METHOD="POST">Username:<INPUT TYPE="TEXT" NAME="username" SIZE="20"><BR />';
 	echo 'Password:<INPUT TYPE="PASSWORD" NAME="password" SIZE="20"><BR />';
 	echo '<INPUT TYPE="SUBMIT" VALUE="Login ..."></FORM></CENTER>';
+}
 ?>
