cf28f2ddf2159ec0a1922f38aee8bbfcb61ae870
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 21:35:33 -1000

    changed location to index pageq

--- gw-logout.php
+++ gw-logout.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_POST['logout'])){
 	session_unset();
 	session_destroy();
-	header("refresh:5;url=gw-login.php");
+	header("refresh:5;url=gw-index.php");
 	echo '<CENTER>You have been logged out ...<BR />Returning to login screen in a few seconds</CENTER>';
 } else {
 	echo 'Something went wrong, you haven\'t been logged out!';
