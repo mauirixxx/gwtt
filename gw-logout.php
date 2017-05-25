d25cfa3b11a35c2ec9c9d34df71b69f7efc4784f
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-24 20:34:33 -1000

    should kill any session data on logout

---
+++ gw-logout.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['logout'])){
+	session_unset();
+	session_destroy();
+	header("refresh:5;url=login.php");
+	echo '<CENTER>You have been logged out ...<BR />Returning to login screen in a few seconds</CENTER>';
+} else {
+	echo 'Something went wrong, you haven't been logged out!';
+}
+?>
