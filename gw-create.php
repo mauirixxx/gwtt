b3a3547b0b1cca351f3b1aa8e95a49ff4da8d0d5
mauirixxx <mauirixxx@users.noreply.github.com>
2017-05-25 16:05:31 -1000

    attempting to validate date submission

--- gw-create.php
+++ gw-create.php
@@ -15,6 +15,13 @@ if ($createnew === "1"){
 	$cname = mysqli_real_escape_string($con, $_POST['cname']);
 	$bdate = mysqli_real_escape_string($con, $_POST['bdate']);
 	$profid = mysqli_real_escape_string($con, $_POST['professionid']);
+	list ($y, $m, $d) = explode('-', $bdate);
+	if (!checkdate($y, $m, $d)) {
+		echo 'Date format is YYYY-MM-DD / 2005-04-28';
+		echo 'Please click <A HREF="gw-create.php">HERE</A> to try again';
+		echo '<BR /><BR />Return to <A HREF="gw-index.php">home</A>.</CENTER></BODY></HTML>';
+		exit();
+	}
 	$sqlcreate = "INSERT INTO `playername` (charname, birthdate, userid, professionid) VALUES ('$cname', '$bdate', $userid, $profid)";
 	echo 'SQL Code w/ variables is: ' . $sqlcreate . '';
 	echo 'Character creation database insertion code here';
