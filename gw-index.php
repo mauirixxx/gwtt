3520c28c8d302e5461e9974bfacfaa182986e7f4
mauirixxx <mauirixxx@users.noreply.github.com>
2017-06-10 01:55:42 -1000

    removed 2 centers, added 1 center

--- gw-index.php
+++ gw-index.php
@@ -8,13 +8,17 @@ if (isset($_SESSION['userid']) && ($_SESSION['access'])){
 	$uname = $_SESSION['username'];
 	echo '<TITLE>Welcome, ' . $uname . '</TITLE></HEAD><BODY><CENTER>';
 	echo 'Proceed to character selection <A HREF="gw-toon.php" CLASS="navlink">here</A><BR />';
-	echo 'Create a new character to record <A HREF="gw-create.php" CLASS="navlink">here</A><BR /></CENTER>';
+	echo 'Create a new character to record <A HREF="gw-create.php" CLASS="navlink">here</A><BR />';
+	if ($_SESSION['access'] == 9){
+		echo'<BR />Hello admin, please click <A HREF="gw-admin.php">here</A> to access the admin page <BR />';
+	}
 } else {
 	echo '<TITLE>Login Required</TITLE></HEAD><BODY>';
 	echo '<CENTER><FORM ACTION="gw-login.php" METHOD="POST">Username:<INPUT TYPE="TEXT" NAME="username" SIZE="20"><BR />';
 	echo 'Password:<INPUT TYPE="PASSWORD" NAME="password" SIZE="20"><BR />';
-	echo '<INPUT TYPE="SUBMIT" VALUE="Login ..."></FORM></CENTER>';
+	echo '<INPUT TYPE="SUBMIT" VALUE="Login ..."></FORM>';
 }
 ?>
+</CENTER>
 </BODY>
 </HTML>
