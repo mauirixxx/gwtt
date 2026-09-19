5e345641e0666a99d688312c475c2fd843dca01e
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-18 15:55:53 -1000

    Update gw-treasure-users.sql
    brokennnnnnnn

--- db/gw-treasure-users.sql
+++ db/gw-treasure-users.sql
@@ -1,4 +1,31 @@
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+/* Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `userid`    INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username`  VARCHAR(50) NOT NULL,
+  `password`  VARCHAR(255) NOT NULL COMMENT 'Stores password_hash() outputs (Bcrypt/Argon2)',
+  `email`     VARCHAR(255) NOT NULL,
+  `access`    TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '0=User, 1=Admin',
+  
+  -- Primary & Unique Constraints
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `uk_username` (`username`),
+  UNIQUE KEY `uk_email` (`email`)
+
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;/*!40101 SET NAMES utf8 */;
 
 /*!40101 SET SQL_MODE=''*/;
 
