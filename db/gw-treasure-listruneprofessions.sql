ce986cb038f7778ebb11a4ad220c6012571ff2d6
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-18 15:42:01 -1000

    Update gw-treasure-listruneprofessions.sql
    breaking more stuff

--- db/gw-treasure-listruneprofessions.sql
+++ db/gw-treasure-listruneprofessions.sql
@@ -1,28 +1,37 @@
-/*
-SQLyog Ultimate - MySQL GUI v8.2 
-MySQL - 5.5.52-MariaDB : Database - mauirixxx
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `listruneprofessions` */
+
+/* Table structure for table `listruneprofessions` */
+
+DROP TABLE IF EXISTS `listruneprofessions`;
 
 CREATE TABLE `listruneprofessions` (
-  `runeprofid` int(2) NOT NULL AUTO_INCREMENT,
-  `runeprofession` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`runeprofid`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+  `runeprofid`      TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `runeprofession`  VARCHAR(30) NOT NULL,
+  
+  -- Primary & Unique Constraints
+  PRIMARY KEY (`runeprofid`),
+  UNIQUE KEY `uk_runeprofession` (`runeprofession`)
 
-/*Data for the table `listruneprofessions` */
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert  into `listruneprofessions`(`runeprofid`,`runeprofession`) values (1,'None'),(2,'Warrior'),(3,'Ranger'),(4,'Monk'),(5,'Necromancer'),(6,'Mesmer'),(7,'Elementalist'),(8,'Assassin'),(9,'Ritualist'),(10,'Paragon'),(11,'Dervish');
+/* Seed data for table `listruneprofessions` */
+
+INSERT INTO `listruneprofessions` (`runeprofid`, `runeprofession`) VALUES
+(1, 'None'),
+(2, 'Warrior'),
+(3, 'Ranger'),
+(4, 'Monk'),
+(5, 'Necromancer'),
+(6, 'Mesmer'),
+(7, 'Elementalist'),
+(8, 'Assassin'),
+(9, 'Ritualist'),
+(10, 'Paragon'),
+(11, 'Dervish');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
