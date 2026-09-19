1a9f9ec2182b9d5a00f8e41bdc35def6f711ea64
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-18 15:47:29 -1000

    Update gw-treasure-materials.sql
    break!

--- db/gw-treasure-materials.sql
+++ db/gw-treasure-materials.sql
@@ -1,24 +1,52 @@
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `materials` */
+
+/* Table structure for table `materials` */
 
 DROP TABLE IF EXISTS `materials`;
 
 CREATE TABLE `materials` (
-  `materialid` int(2) NOT NULL AUTO_INCREMENT,
-  `material` varchar(30) DEFAULT NULL COMMENT 'what kind of rare mat dropped: ecto, amber, silk, claw, ink, etc',
-  PRIMARY KEY (`materialid`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+  `materialid`  TINYINT(3) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `material`    VARCHAR(50) NOT NULL COMMENT 'Rare/Common crafting material drops',
+  
+  -- Primary & Unique Constraints
+  PRIMARY KEY (`materialid`),
+  UNIQUE KEY `uk_material` (`material`)
 
-/*Data for the table `materials` */
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-insert  into `materials`(`materialid`,`material`) values (1,'No material drop'),(2,'Amber Chunk'),(3,'Bolt of Damask'),(4,'Bolt of Linen'),(5,'Bolt of Silk'),(6,'Deldrimor Steel Ingot'),(7,'Diamond'),(8,'Elonian Leather Square'),(9,'Fur Square'),(10,'Glob of Ectoplasm'),(11,'Jadeite Shard'),(12,'Leather Square'),(13,'Lump of Charcoal'),(14,'Monstrous Claw'),(15,'Monstrous Eye'),(16,'Monstrous Fang'),(17,'Obsidian Shard'),(18,'Onyx Gemstone'),(19,'Roll of Parchment'),(20,'Roll of Vellum'),(21,'Ruby'),(22,'Sapphire'),(23,'Spiritwood Plank'),(24,'Steel Ingot'),(25,'Tempered Glass Vial'),(26,'Vial of Ink');
+/* Seed data for table `materials` */
+
+INSERT INTO `materials` (`materialid`, `material`) VALUES
+(1, 'No material drop'),
+(2, 'Amber Chunk'),
+(3, 'Bolt of Damask'),
+(4, 'Bolt of Linen'),
+(5, 'Bolt of Silk'),
+(6, 'Deldrimor Steel Ingot'),
+(7, 'Diamond'),
+(8, 'Elonian Leather Square'),
+(9, 'Fur Square'),
+(10, 'Glob of Ectoplasm'),
+(11, 'Jadeite Shard'),
+(12, 'Leather Square'),
+(13, 'Lump of Charcoal'),
+(14, 'Monstrous Claw'),
+(15, 'Monstrous Eye'),
+(16, 'Monstrous Fang'),
+(17, 'Obsidian Shard'),
+(18, 'Onyx Gemstone'),
+(19, 'Roll of Parchment'),
+(20, 'Roll of Vellum'),
+(21, 'Ruby'),
+(22, 'Sapphire'),
+(23, 'Spiritwood Plank'),
+(24, 'Steel Ingot'),
+(25, 'Tempered Glass Vial'),
+(26, 'Vial of Ink');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
