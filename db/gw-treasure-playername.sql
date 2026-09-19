681b163cb7bb8c1e22b604878cc05c22d129903d
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-18 15:51:49 -1000

    Update gw-treasure-playername.sql
    didn't know the OG code allowed for duplicate character names 😯

--- db/gw-treasure-playername.sql
+++ db/gw-treasure-playername.sql
@@ -1,29 +1,39 @@
-/*
-SQLyog Ultimate - MySQL GUI v8.2 
-MySQL - 5.5.52-MariaDB : Database - mauirixxx
-*********************************************************************
-*/
-
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `playername` */
+
+/* Table structure for table `playername` */
+
+DROP TABLE IF EXISTS `playername`;
 
 CREATE TABLE `playername` (
-  `playerid` int(11) NOT NULL AUTO_INCREMENT,
-  `charname` varchar(19) DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
-  `userid` int(11) DEFAULT NULL,
-  `professionid` int(2) DEFAULT NULL COMMENT 'this is taken from the listruneprofessions table',
-  `profcolor` char(4) DEFAULT NULL,
-  PRIMARY KEY (`playerid`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `playerid`      INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `charname`      VARCHAR(30) NOT NULL,
+  `birthdate`     DATE DEFAULT NULL,
+  `userid`        INT(11) UNSIGNED NOT NULL,
+  `professionid`  TINYINT(3) UNSIGNED NOT NULL COMMENT 'References listruneprofessions(runeprofid)',
+  `profcolor`     VARCHAR(7) DEFAULT '#000000',
+  
+  -- Primary & Unique Constraints
+  PRIMARY KEY (`playerid`),
+  UNIQUE KEY `uk_charname` (`charname`),
+  
+  -- Performance Indexes
+  INDEX `idx_user_player` (`userid`),
+  INDEX `idx_profession` (`professionid`),
+
+  -- Foreign Key Constraints
+  CONSTRAINT `fk_player_user`
+    FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+
+  CONSTRAINT `fk_player_profession`
+    FOREIGN KEY (`professionid`) REFERENCES `listruneprofessions` (`runeprofid`)
+    ON DELETE RESTRICT ON UPDATE CASCADE
+
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
