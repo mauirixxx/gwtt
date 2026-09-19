2a2691afde7d8da0ca26e4a57e3831f30273d7e2
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-18 15:02:51 -1000

    Update gw-record.php
    oh my

--- gw-record.php
+++ gw-record.php
@@ -1,155 +1,212 @@
-<!DOCTYPE html>
-<HTML>
-<HEAD>
-<link rel="stylesheet" type="text/css" href="gw-style.css">
-<TITLE>What Dropped?</TITLE>
-</HEAD>
-<BODY>
 <?php
 session_start();
-include_once 'gw-connect.php';
+require_once 'gw-connect.php';
+
+// 1. Auth Check
+if (!isset($_SESSION['playerid']) \vert{}\vert{} empty($_SESSION['playerid'])) {
+    header('Location: login.php');
+    exit;
+}
+
+// 2. Database Connection
 $con = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME);
-$toonid = $_SESSION['playerid'];
-$profcolor = $_SESSION['profcolor'];
-$location = mysqli_real_escape_string($con, $_POST['locationid']);
-$whatdropped = mysqli_real_escape_string($con, $_POST['gwdrop']);
-if ($con->connect_errno > 0){
-	die ('Unable to connect to database [' . $db->connect_errno . ']');
+if ($con->connect_errno) {
+    error_log("Database error: " . $con->connect_error);
+    die("Database connection failed.");
 }
-echo '<STYLE TYPE="TEXT/CSS" MEDIA="SCREEN">body { background-color: ' . $profcolor . '; }</STYLE>';
-echo '<CENTER>At ';
-$sqlmaplocation = "SELECT * FROM `treasuredata` WHERE `treasureid` = $location";
-if (!$result = $con->query($sqlmaplocation)){
-	die ('There was an error running the query [' . $con->error . ']');
+
+// 3. Inputs & Validation
+$toonid    = (int)$_SESSION['playerid'];$location  = isset($_POST['locationid']) ? (int)$_POST['locationid'] : 0;
+$whatdropped = isset($_POST['gwdrop']) ? (int)$_POST['gwdrop'] : 0;
+
+$profcolor = isset($_SESSION['profcolor']) ?$_SESSION['profcolor'] : '#ffffff';
+if (!preg_match('/^#[a-fA-F0-9]{6}$/', $profcolor)) {$profcolor = '#ffffff';
 }
-while ($row = $result->fetch_array()){
-	$locname = $row['location'];
-	$loclink = $row['wikilink'];
-	$locid = $row['treasureid'];
-	echo '<A HREF="' . $loclink . '" CLASS="navlink">' . $locname . '</A> (Guild Wars Wiki link)';
-}
-echo '</CENTER>';
-if ($whatdropped == "1"){
-	echo '<CENTER><FORM METHOD="POST" ACTION="gw-insert.php">';
-	echo 'on <INPUT NAME="treasuredate" TYPE="DATE" PLACEHOLDER="2006-10-26"> a ';
-	//code for white blue purple etc
-	$sqlweaprare = "SELECT * FROM `listrarity` ORDER BY `rareid` ASC";
-	if (!$result = $con->query($sqlweaprare)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo '<SELECT NAME="rare">';
-	while ($row = $result->fetch_array()){
-		$rareid = $row['rareid'];
-		$rarity = $row['rarity'];
-		echo '<OPTION VALUE="' . $rareid . '">' . $rarity . '</OPTION>';
-	}
-	echo '</SELECT>, ';
-	//code for weapon attribute requirment
-	$sqlweapreq = "SELECT * FROM `listreq` ORDER BY `req` ASC";
-	if (!$result = $con->query($sqlweapreq)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo 'req<SELECT NAME="requirement">';
-	while ($row = $result->fetch_array()){
-		$reqid = $row['req'];
-		echo '<OPTION VALUE="' . $reqid . '">' . $reqid . '</OPTION>';
-	}
-	echo '</SELECT>';
-	//code for what attribute the weapon is (command, axe mastery, energy storage, etc
-	$sqlweapattr = "SELECT * FROM `listattribute` ORDER BY `weapattrid` ASC";
-	if (!$result = $con->query($sqlweapattr)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo '<SELECT NAME="attribute">';
-	while ($row = $result->fetch_array()){
-		$attrid = $row['weapattrid'];
-		$weapattr = $row['weaponattribute'];
-		echo '<OPTION VALUE="' . $attrid . '">' . $weapattr . '</OPTION>';
-	//need to add a nested while loop, to preselect the weapon with the attribute, or somehow java it? An r9 Axe of Energy Storage combo doesn't exist.
-	}
-	echo '</SELECT>';
-	//code for what the weapon is - staff, dagger, scythe, wand, sword, etc
-	$sqlweaptype = "SELECT * FROM `listtype` ORDER BY `weaponid` ASC";
-	if (!$result = $con->query($sqlweaptype)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo '<SELECT NAME="weapon">';
-	while ($row = $result->fetch_array()){
-		$typeid = $row['weaponid'];
-		$weapon = $row['weapontype'];
-		echo '<OPTION VALUE="' . $typeid . '">' . $weapon . '</OPTION>';
-	}
-	echo '</SELECT> called the <INPUT TYPE="TEXT" NAME="itemname" MAXLENGTH="100" SIZE="40">';
-	echo ' and <INPUT TYPE="NUMBER" NAME="droppedgold" SIZE="4" MIN="1" MAX="9999"> gold pieces.';
-	echo '<INPUT TYPE="HIDDEN" NAME="droptype" VALUE="1"><INPUT TYPE="HIDDEN" NAME="location" VALUE="' . $locid .'">';
-	echo '<INPUT TYPE="HIDDEN" NAME="chartoon" VALUE="' . $toonid .'">';
-	echo ' <BR /><INPUT TYPE="SUBMIT" VALUE="Click me!"></FORM></CENTER><BR />';
-} else if ($whatdropped == "2"){
-	echo '<CENTER><FORM METHOD="POST" ACTION="gw-insert.php">';
-	echo 'on <INPUT NAME="treasuredate" TYPE="DATE" PLACEHOLDER="2006-10-26"> a ';
-	//code for what rare material dropped
-	$sqlraremat = "SELECT * FROM `materials` ORDER BY `materialid` ASC";
-	if (!$result = $con->query($sqlraremat)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo '<SELECT NAME="rarematerial">';
-	while ($row = $result->fetch_array()){
-		$matid = $row['materialid'];
-		$raremat = $row['material'];
-		echo '<OPTION VALUE="' . $matid . '">' . $raremat . '</OPTION>';
-	}
-	echo '</SELECT> ';
-	echo ' and <INPUT TYPE="NUMBER" NAME="droppedgold" SIZE="4" MIN="1" MAX="9999"> gold pieces.';
-	echo '<INPUT TYPE="HIDDEN" NAME="droptype" VALUE="2"><INPUT TYPE="HIDDEN" NAME="location" VALUE="' . $locid .'">';
-	echo '<INPUT TYPE="HIDDEN" NAME="chartoon" VALUE="' . $toonid .'">';
-	echo ' <BR /><INPUT TYPE="SUBMIT" VALUE="Click me!"></FORM></CENTER><BR />';
-} else if ($whatdropped == "3"){
-	echo '<CENTER><FORM METHOD="POST" ACTION="gw-insert.php">';
-	echo 'on <INPUT NAME="treasuredate" TYPE="DATE" PLACEHOLDER="2006-10-26"> a ';
-	echo '<SELECT NAME="runerarity"><OPTION VALUE="2">Blue</OPTION><OPTION VALUE="3">Purple</OPTION><OPTION VALUE="4">Gold</OPTION></SELECT> ';
-	//code for what rune dropped
-	$sqlrune = "SELECT * FROM `listrunes` ORDER BY `runeid` ASC";
-	if (!$result = $con->query($sqlrune)){
-		die ('There was an error running the query [' . $con->error . ']');
-	}
-	echo 'rune of <SELECT NAME="rune">';
-	while ($row = $result->fetch_array()){
-		$runeid = $row['runeid'];
-		$rune = $row['runes'];
-		echo '<OPTION VALUE="' . $runeid . '">' . $rune . '</OPTION>';
-	}
-	echo '</SELECT> ';
-	echo ' and <INPUT TYPE="NUMBER" NAME="droppedgold" SIZE="4" MIN="0" MAX="9999"> gold pieces.';
-	echo '<INPUT TYPE="HIDDEN" NAME="droptype" VALUE="3"><INPUT TYPE="HIDDEN" NAME="location" VALUE="' . $locid .'">';
-	echo '<INPUT TYPE="HIDDEN" NAME="chartoon" VALUE="' . $toonid .'">';
-	echo ' <BR /><INPUT TYPE="SUBMIT" VALUE="Click me!"></FORM></CENTER><BR />';
-} else if ($whatdropped == "4"){
-	echo '<CENTER><FORM METHOD="POST" ACTION="gw-insert.php">';
-	echo 'on <INPUT NAME="treasuredate" TYPE="DATE" PLACEHOLDER="2006-10-26" VALUE="' . date('Y-m-d') . '"> nothing dropped! Maybe try again in 30 days on: ' . date('Y-m-d', strtotime("+30 days"));
-	echo '<INPUT TYPE="HIDDEN" NAME="droppedgold" VALUE="0"><INPUT TYPE="HIDDEN" NAME="itemname" VALUE="Nothing dropped!">';
-	echo '<INPUT TYPE="HIDDEN" NAME="droptype" VALUE="4"><INPUT TYPE="HIDDEN" NAME="location" VALUE="' . $locid .'">';
-	echo '<INPUT TYPE="HIDDEN" NAME="itemtype" VALUE="17"><INPUT TYPE="HIDDEN" NAME="chartoon" VALUE="' . $toonid .'">';
-	echo ' <BR /><INPUT TYPE="SUBMIT" VALUE="Click me!"></FORM></CENTER><BR />';
-} else {
-	echo '<CENTER><FORM METHOD="POST"><SELECT NAME="gwdrop" onchange="this.form.submit()">';
-	echo '<OPTION SELECTED DISABLED>choose one</OPTION>';
-	echo '<OPTION VALUE="1">Weapon</OPTION>';
-	echo '<OPTION VALUE="2">Rare Material</OPTION>';
-	echo '<OPTION VALUE="3">Rune</OPTION>';
-	echo '<OPTION VALUE="4">Nothing!</OPTION></SELECT>';
-	echo '<INPUT TYPE="HIDDEN" NAME="locationid" VALUE="' . $location . '">';
-	echo '<INPUT TYPE="HIDDEN" NAME="playerid" VALUE="' . $toonid .'">';
-	echo '<NOSCRIPT><INPUT TYPE="SUBMIT" VALUE="SUBMIT"></NOSCRIPT></FORM></CENTER>';
+
+// 4. Prepared Statement for Location Query
+$locname = '';
+$loclink = '';$locid   = 0;
+
+if ($location > 0) {
+    $stmt =$con->prepare("SELECT treasureid, location, wikilink FROM `treasuredata` WHERE `treasureid` = ?");
+    $stmt->bind_param("i", $location);$stmt->execute();
+    $result =$stmt->get_result();
+    
+    if ($row =$result->fetch_assoc()) {
+        $locid   = (int)$row['treasureid'];
+        $locname =$row['location'];
+        $loclink =$row['wikilink'];
+    }
+    $stmt->close();
 }
 ?>
-<BR />
-<CENTER>
-<FORM METHOD="POST" ACTION="gw-location.php">
-<INPUT TYPE="HIDDEN" NAME="cnameid" VALUE="0">
-<INPUT TYPE="SUBMIT" VALUE="Return to location selection">
-</FORM>
-</CENTER>
-<!-- really need to make a footer page for this -->
-<BR /><BR /><CENTER><FORM METHOD="POST" ACTION="gw-logout.php"><INPUT TYPE="HIDDEN" NAME="logout"><INPUT TYPE="SUBMIT" VALUE="Logout"></FORM></CENTER>
-</BODY>
-</HTML>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="gw-style.css">
+    <title>What Dropped?</title>
+    <style media="screen">
+        body { background-color: <?php echo htmlspecialchars($profcolor, ENT_QUOTES, 'UTF-8'); ?>; }
+    </style>
+</head>
+<body>
+
+<div style="text-align: center;">
+<?php if ($locid > 0): ?>
+    At <a href="<?php echo htmlspecialchars($loclink, ENT_QUOTES, 'UTF-8'); ?>" class="navlink">
+        <?php echo htmlspecialchars($locname, ENT_QUOTES, 'UTF-8'); ?>
+    </a> (Guild Wars Wiki link)
+<?php else: ?>
+    <p>Invalid location selected.</p>
+<?php endif; ?>
+</div>
+
+<br />
+
+<?php if ($whatdropped === 1): ?>
+    <!-- WEAPON FORM -->
+    <div style="text-align: center;">
+        <form method="POST" action="gw-insert.php">
+            on <input name="treasuredate" type="date" value="<?php echo date('Y-m-d'); ?>"> a 
+            
+            <select name="rare">
+                <?php
+                $res =$con->query("SELECT rareid, rarity FROM `listrarity` ORDER BY `rareid` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['rareid'] . '">' . htmlspecialchars($row['rarity'], ENT_QUOTES, 'UTF-8') . '</option>';
+                }
+                ?>
+            </select>, 
+
+            req<select name="requirement">
+                <?php
+                $res =$con->query("SELECT req FROM `listreq` ORDER BY `req` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['req'] . '">' . (int)$row['req'] . '</option>';
+                }
+                ?>
+            </select>
+
+            <select name="attribute">
+                <?php
+                $res =$con->query("SELECT weapattrid, weaponattribute FROM `listattribute` ORDER BY `weapattrid` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['weapattrid'] . '">' . htmlspecialchars($row['weaponattribute'], ENT_QUOTES, 'UTF-8') . '</option>';
+                }
+                ?>
+            </select>
+
+            <select name="weapon">
+                <?php
+                $res =$con->query("SELECT weaponid, weapontype FROM `listtype` ORDER BY `weaponid` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['weaponid'] . '">' . htmlspecialchars($row['weapontype'], ENT_QUOTES, 'UTF-8') . '</option>';
+                }
+                ?>
+            </select> 
+            called the <input type="text" name="itemname" maxlength="100" size="40">
+            and <input type="number" name="droppedgold" min="1" max="9999"> gold pieces.
+            
+            <input type="hidden" name="droptype" value="1">
+            <input type="hidden" name="location" value="<?php echo $locid; ?>">
+            <input type="hidden" name="chartoon" value="<?php echo $toonid; ?>">
+            <br /><input type="submit" value="Submit Drop">
+        </form>
+    </div>
+
+<?php elseif ($whatdropped === 2): ?>
+    <!-- RARE MATERIAL FORM -->
+    <div style="text-align: center;">
+        <form method="POST" action="gw-insert.php">
+            on <input name="treasuredate" type="date" value="<?php echo date('Y-m-d'); ?>"> a 
+            <select name="rarematerial">
+                <?php
+                $res =$con->query("SELECT materialid, material FROM `materials` ORDER BY `materialid` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['materialid'] . '">' . htmlspecialchars($row['material'], ENT_QUOTES, 'UTF-8') . '</option>';
+                }
+                ?>
+            </select> 
+            and <input type="number" name="droppedgold" min="1" max="9999"> gold pieces.
+            <input type="hidden" name="droptype" value="2">
+            <input type="hidden" name="location" value="<?php echo $locid; ?>">
+            <input type="hidden" name="chartoon" value="<?php echo $toonid; ?>">
+            <br /><input type="submit" value="Submit Drop">
+        </form>
+    </div>
+
+<?php elseif ($whatdropped === 3): ?>
+    <!-- RUNE FORM -->
+    <div style="text-align: center;">
+        <form method="POST" action="gw-insert.php">
+            on <input name="treasuredate" type="date" value="<?php echo date('Y-m-d'); ?>"> a 
+            <select name="runerarity">
+                <option value="2">Blue</option>
+                <option value="3">Purple</option>
+                <option value="4">Gold</option>
+            </select> 
+            rune of 
+            <select name="rune">
+                <?php
+                $res =$con->query("SELECT runeid, runes FROM `listrunes` ORDER BY `runeid` ASC");
+                while ($row =$res->fetch_assoc()) {
+                    echo '<option value="' . (int)$row['runeid'] . '">' . htmlspecialchars($row['runes'], ENT_QUOTES, 'UTF-8') . '</option>';
+                }
+                ?>
+            </select> 
+            and <input type="number" name="droppedgold" min="0" max="9999"> gold pieces.
+            <input type="hidden" name="droptype" value="3">
+            <input type="hidden" name="location" value="<?php echo $locid; ?>">
+            <input type="hidden" name="chartoon" value="<?php echo $toonid; ?>">
+            <br /><input type="submit" value="Submit Drop">
+        </form>
+    </div>
+
+<?php elseif ($whatdropped === 4): ?>
+    <!-- NOTHING DROPPED FORM -->
+    <div style="text-align: center;">
+        <form method="POST" action="gw-insert.php">
+            on <input name="treasuredate" type="date" value="<?php echo date('Y-m-d'); ?>"> nothing dropped!
+            <input type="hidden" name="droppedgold" value="0">
+            <input type="hidden" name="itemname" value="Nothing dropped!">
+            <input type="hidden" name="droptype" value="4">
+            <input type="hidden" name="location" value="<?php echo $locid; ?>">
+            <input type="hidden" name="itemtype" value="17">
+            <input type="hidden" name="chartoon" value="<?php echo $toonid; ?>">
+            <br /><input type="submit" value="Submit Drop">
+        </form>
+    </div>
+
+<?php else: ?>
+    <!-- SELECT DROP TYPE -->
+    <div style="text-align: center;">
+        <form method="POST">
+            <select name="gwdrop" onchange="this.form.submit()">
+                <option selected disabled>Choose drop type</option>
+                <option value="1">Weapon</option>
+                <option value="2">Rare Material</option>
+                <option value="3">Rune</option>
+                <option value="4">Nothing!</option>
+            </select>
+            <input type="hidden" name="locationid" value="<?php echo $location; ?>">
+            <noscript><input type="submit" value="Submit"></noscript>
+        </form>
+    </div>
+<?php endif; ?>
+
+<br />
+<div style="text-align: center;">
+    <form method="POST" action="gw-location.php">
+        <input type="submit" value="Return to location selection">
+    </form>
+    <br /><br />
+    <form method="POST" action="gw-logout.php">
+        <input type="hidden" name="logout" value="1">
+        <input type="submit" value="Logout">
+    </form>
+</div>
+
+</body>
+</html>
+<?php $con->close(); ?>
