689a276c3b0a27a44f7b487a5c9e3efda1185359
mauirixxx <mauirixxx@users.noreply.github.com>
2026-09-19 21:30:52 -1000

    Add insignias and weapon attribute map to master schema

--- db/master-schema.sql
+++ db/master-schema.sql
@@ -6,13 +6,25 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS history; DROP TABLE IF EXISTS playername; DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS listrunes; DROP TABLE IF EXISTS listruneprofessions; DROP TABLE IF EXISTS listattribute;
+DROP TABLE IF EXISTS weapon_attribute_map; DROP TABLE IF EXISTS listinsignias; DROP TABLE IF EXISTS listrunes; DROP TABLE IF EXISTS listruneprofessions; DROP TABLE IF EXISTS listattribute;
 DROP TABLE IF EXISTS listrarity; DROP TABLE IF EXISTS listreq; DROP TABLE IF EXISTS listtype;
 DROP TABLE IF EXISTS materials; DROP TABLE IF EXISTS treasuredata;
 SET FOREIGN_KEY_CHECKS=1;
 
 CREATE TABLE listruneprofessions (runeprofid TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,runeprofession VARCHAR(30) NOT NULL,PRIMARY KEY(runeprofid),UNIQUE KEY uk_runeprofession(runeprofession)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 INSERT INTO listruneprofessions VALUES (1,'None'),(2,'Warrior'),(3,'Ranger'),(4,'Monk'),(5,'Necromancer'),(6,'Mesmer'),(7,'Elementalist'),(8,'Assassin'),(9,'Ritualist'),(10,'Paragon'),(11,'Dervish');
+
+CREATE TABLE listinsignias (insigniaid TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,professionid TINYINT UNSIGNED NOT NULL,insignia VARCHAR(60) NOT NULL,PRIMARY KEY(insigniaid),UNIQUE KEY uk_insignia(insignia),KEY idx_insignia_profession(professionid),CONSTRAINT fk_insignia_profession FOREIGN KEY(professionid) REFERENCES listruneprofessions(runeprofid) ON DELETE RESTRICT ON UPDATE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO listinsignias(professionid,insignia) VALUES
+(1,'Survivor Insignia'),(1,'Radiant Insignia'),(1,'Stalwart Insignia'),(1,'Brawler''s Insignia'),(1,'Blessed Insignia'),(1,'Herald''s Insignia'),(1,'Sentry''s Insignia'),
+(2,'Knight''s Insignia'),(2,'Stonefist Insignia'),(2,'Dreadnought Insignia'),(2,'Sentinel''s Insignia'),(2,'Lieutenant''s Insignia'),
+(3,'Frostbound Insignia'),(3,'Pyrebound Insignia'),(3,'Stormbound Insignia'),(3,'Scout''s Insignia'),(3,'Earthbound Insignia'),(3,'Beastmaster''s Insignia'),
+(4,'Wanderer''s Insignia'),(4,'Disciple''s Insignia'),(4,'Anchorite''s Insignia'),
+(5,'Bloodstained Insignia'),(5,'Tormentor''s Insignia'),(5,'Bonelace Insignia'),(5,'Minion Master''s Insignia'),(5,'Blighter''s Insignia'),(5,'Undertaker''s Insignia'),
+(6,'Virtuoso''s Insignia'),(6,'Artificer''s Insignia'),(6,'Prodigy''s Insignia'),
+(7,'Hydromancer Insignia'),(7,'Geomancer Insignia'),(7,'Pyromancer Insignia'),(7,'Aeromancer Insignia'),(7,'Prismatic Insignia'),
+(8,'Vanguard''s Insignia'),(8,'Infiltrator''s Insignia'),(8,'Saboteur''s Insignia'),(8,'Nightstalker''s Insignia'),
+(9,'Shaman''s Insignia'),(9,'Ghost Forge Insignia'),(9,'Mystic''s Insignia'),(10,'Centurion''s Insignia'),(11,'Windwalker Insignia'),(11,'Forsaken Insignia');
 
 CREATE TABLE listrunes (runeid TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,runeclassid TINYINT UNSIGNED NOT NULL,runes VARCHAR(50) NOT NULL,PRIMARY KEY(runeid),UNIQUE KEY uk_rune_class(runeclassid,runes),CONSTRAINT fk_runes_profession FOREIGN KEY(runeclassid) REFERENCES listruneprofessions(runeprofid) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 INSERT INTO listrunes VALUES
@@ -33,6 +45,13 @@ CREATE TABLE listreq (req TINYINT UNSIGNED NOT NULL,PRIMARY KEY(req)) ENGINE=Inn
 INSERT INTO listreq VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13);
 CREATE TABLE listtype (weaponid TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,weapontype VARCHAR(40) NOT NULL,PRIMARY KEY(weaponid),UNIQUE KEY uk_weapontype(weapontype)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 INSERT INTO listtype(weapontype) VALUES ('Axe'),('Hammer'),('Sword'),('Dagger'),('Scythe'),('Bow (Flatbow)'),('Bow (Hornbow)'),('Bow (Longbow)'),('Bow (Recurve)'),('Bow (Shortbow)'),('Spear'),('Staff'),('Wand'),('Focus'),('Shield');
+CREATE TABLE weapon_attribute_map (weaponid TINYINT UNSIGNED NOT NULL,weapattrid TINYINT UNSIGNED NOT NULL,PRIMARY KEY(weaponid,weapattrid),KEY idx_weapon_attribute_attr(weapattrid),CONSTRAINT fk_weapon_attribute_type FOREIGN KEY(weaponid) REFERENCES listtype(weaponid) ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT fk_weapon_attribute_attribute FOREIGN KEY(weapattrid) REFERENCES listattribute(weapattrid) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO weapon_attribute_map(weaponid,weapattrid)
+SELECT t.weaponid,a.weapattrid FROM listtype t JOIN listattribute a ON
+(t.weapontype='Axe' AND a.weaponattribute='Axe Mastery') OR (t.weapontype='Hammer' AND a.weaponattribute='Hammer Mastery') OR (t.weapontype='Sword' AND a.weaponattribute='Swordsmanship') OR (t.weapontype='Dagger' AND a.weaponattribute='Dagger Mastery') OR (t.weapontype='Scythe' AND a.weaponattribute='Scythe Mastery') OR (t.weapontype LIKE 'Bow (%)' AND a.weaponattribute='Marksmanship') OR (t.weapontype='Spear' AND a.weaponattribute='Spear Mastery') OR
+(t.weapontype='Shield' AND a.weaponattribute IN ('Strength','Tactics','Leadership','Command','Motivation')) OR
+(t.weapontype IN ('Focus','Wand','Staff') AND a.weaponattribute IN ('Divine Favor','Healing Prayers','Protection Prayers','Smiting Prayers','Soul Reaping','Blood Magic','Curses','Death Magic','Fast Casting','Inspiration Magic','Domination Magic','Illusion Magic','Energy Storage','Fire Magic','Water Magic','Air Magic','Earth Magic','Spawning Power','Channeling Magic','Communing','Restoration Magic'));
+
 CREATE TABLE materials (materialid TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,material VARCHAR(50) NOT NULL,PRIMARY KEY(materialid),UNIQUE KEY uk_material(material)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 INSERT INTO materials(materialid,material) VALUES (1,'No material drop'),(2,'Amber Chunk'),(3,'Bolt of Damask'),(4,'Bolt of Linen'),(5,'Bolt of Silk'),(6,'Deldrimor Steel Ingot'),(7,'Diamond'),(8,'Elonian Leather Square'),(9,'Fur Square'),(10,'Glob of Ectoplasm'),(11,'Jadeite Shard'),(12,'Leather Square'),(13,'Lump of Charcoal'),(14,'Monstrous Claw'),(15,'Monstrous Eye'),(16,'Monstrous Fang'),(17,'Obsidian Shard'),(18,'Onyx Gemstone'),(19,'Roll of Parchment'),(20,'Roll of Vellum'),(21,'Ruby'),(22,'Sapphire'),(23,'Spiritwood Plank'),(24,'Steel Ingot'),(25,'Tempered Glass Vial'),(26,'Vial of Ink');
 CREATE TABLE treasuredata (treasureid SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,location VARCHAR(100) NOT NULL,wikilink VARCHAR(255),PRIMARY KEY(treasureid),UNIQUE KEY uk_location(location)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -52,7 +71,7 @@ CREATE TABLE `history` (
   `charnameid` INT UNSIGNED NOT NULL,
   `locationid` SMALLINT UNSIGNED NOT NULL,
   `goldrec` MEDIUMINT UNSIGNED NOT NULL DEFAULT 0,
-  `drop_type` TINYINT UNSIGNED NOT NULL COMMENT '1=Weapon, 2=Material, 3=Rune, 4=Nothing',
+  `drop_type` TINYINT UNSIGNED NOT NULL COMMENT '1=Weapon, 2=Material, 3=Rune/Insignia, 4=Nothing',
   `material` TINYINT UNSIGNED DEFAULT NULL,
   `itemreq` TINYINT UNSIGNED DEFAULT NULL,
   `itemtype` TINYINT UNSIGNED DEFAULT NULL,
@@ -60,10 +79,12 @@ CREATE TABLE `history` (
   `itemrarity` TINYINT UNSIGNED DEFAULT NULL,
   `itemname` VARCHAR(150) DEFAULT NULL,
   `runetype` TINYINT UNSIGNED DEFAULT NULL,
+  `insignia` TINYINT UNSIGNED DEFAULT NULL,
   PRIMARY KEY (`historyid`),
   INDEX `idx_user_char` (`userid`,`charnameid`),
   INDEX `idx_historydate` (`historydate`),
   INDEX `idx_location` (`locationid`),
+  INDEX `idx_history_insignia` (`insignia`),
   CONSTRAINT `fk_history_user` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_history_character` FOREIGN KEY (`charnameid`) REFERENCES `playername` (`playerid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_history_location` FOREIGN KEY (`locationid`) REFERENCES `treasuredata` (`treasureid`) ON DELETE RESTRICT ON UPDATE CASCADE,
@@ -73,5 +94,6 @@ CREATE TABLE `history` (
   CONSTRAINT `fk_history_attribute` FOREIGN KEY (`itemattribute`) REFERENCES `listattribute` (`weapattrid`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_history_rarity` FOREIGN KEY (`itemrarity`) REFERENCES `listrarity` (`rareid`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_history_rune` FOREIGN KEY (`runetype`) REFERENCES `listrunes` (`runeid`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_history_insignia` FOREIGN KEY (`insignia`) REFERENCES `listinsignias` (`insigniaid`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `chk_history_drop_type` CHECK (`drop_type` BETWEEN 1 AND 4)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
